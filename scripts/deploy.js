e9f894fce8bfa1f10688eba10e274b82382e8c77
ANISH-SINGH <anishraaz90@gmail.com>
2025-04-25 00:37:19 +0530

    update constant.js, utils/index.js, scripts/deploy.js

--- scripts/deploy.js
+++ scripts/deploy.js
@@ -5,6 +5,33 @@ const tokens = (nToken) => {
 };
 
 async function main(params) {
-  //DEPLOYED
-  kdsj;
+  //DEPLOY TOKEN CONTRACT
+  const _initialSupply = tokens(50000000);
+
+  const TheBlockchainCoders = await hre.ethers.getContractFactory(
+    "TheBlockchainCoders"
+  );
+
+  const theBlockchainCoders = await TheBlockchainCoders.deploy(_initialSupply);
+
+  await theBlockchainCoders.deployed();
+  console.log(`TheBlockchainCoders: ${theBlockchainCoders.address}`);
+
+  //TOKEN SALE CONTRACT
+  const _tokenPrice = tokens(1);
+
+  const TokenSale = await hre.ethers.getContractFactory("TokenSale");
+
+  const tokenSale = await TokenSale.deploy(
+    theBlockchainCoders.address,
+    _tokenPrice
+  );
+
+  await tokenSale.deployed();
+  console.log(`TokenSale: ${tokenSale.address}`);
 }
+
+main().catch((error) => {
+  console.error(error);
+  process.exitCode = 1;
+});
