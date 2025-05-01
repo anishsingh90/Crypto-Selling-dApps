7033699addb33f2b28508090ce0ee9b805995064
ANISH-SINGH <anishraaz90@gmail.com>
2025-05-01 11:07:54 +0530

    Start building Frontend

--- pages/_app.js
+++ pages/_app.js
@@ -7,6 +7,22 @@ export default function App({ Component, pageProps }) {
       <StateContextProvider>
         <Component {...pageProps} />
       </StateContextProvider>
+
+      <script src="assests/js/jquery-1.12.4.min.js"></script>
+      <script src="assests/bootstrap/js/bootstrap.min.js"></script>
+      <script src="assests/owlcarausel/js.owl.carousel.min.js"></script>
+      <script src="assests/js/magnific-popup.min.js"></script>
+      <script src="assests/js/waypoints.min.js"></script>
+      <script src="assests/js/parallax.js"></script>
+      <script src="assests/js/jquery.countdown.min.js"></script>
+      <script src="assests/js/particles.min.js"></script>
+      <script src="assests/js/jquery.dd.min.js"></script>
+      <script src="assests/js/jquery.counterup.min.js"></script>
+      <script src="assests/js/spop.min.js"></script>
+      <script src="assests/js/notification.js"></script>
+      <script src="assests/js/scripts.js"></script>
+      <script src="index.js"></script>
+      <script src="assests/js/parallax.js"></script>
     </>
   );
 }
