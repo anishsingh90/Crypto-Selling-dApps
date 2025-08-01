b5282870b4664f606ec7d3ecbd5632bfc6567dd7
Anish Kumar <132129265+anishsingh90@users.noreply.github.com>
2025-08-01 09:38:56 +0530

    Update _app.js

--- pages/_app.js
+++ pages/_app.js
@@ -4,7 +4,7 @@ import { StateContextProvider } from "../Context/index";
 export default function App({ Component, pageProps }) {
   return (
     <>
-      <StateContextProvider>
+      <StateContextProvider> 
         <Component {...pageProps} />
       </StateContextProvider>
 
